1eafbe50b97f1014048ad115e54eeab67abae46e
Win Htaik Aung <winhtaikaung28@hotmail.com>
2022-04-28 12:07:15 +0800

    Update Search Insert Position.js

--- lc/array/easy/Search Insert Position.js
+++ lc/array/easy/Search Insert Position.js
@@ -26,3 +26,35 @@ var searchInsert = function(nums, target) {
  }   
     
 };
+
+// ဒီ Approach ကတော့ Binaray Search နဲ့ လုပ်ထားတာပါ အပေါ်က ထက် ၂၀ms ပိုမြန်ပါတယ်။ သူကတော့ အလယ်ကဖြတ်ဖြတ်ပြီး ရှိတဲ့ အပိုင်း ကို ရှာသွားတာမို့လို့ ပိုမြန်ပါတယ်
+var searchInsertBinarySearchApproach = function(nums, target) {
+
+ function Comparator(arr,left,right,target){
+   
+   const mid = Math.round((left+right)/2)
+
+     if (arr[mid] === target){
+         return mid
+     }
+
+     if (left>right){ // ဒီနေရာကတော့ ကိုယ့် target က Array ထဲမှာ ရှာမတွေ့ရင် Left Index ကိုပြန်ပေးမှာပါ
+         return left
+     }else if(arr[mid]>target){ // ဒီနေရာကတော့ ကိုဘ်ရှာချင်တဲ့ Target ဘယ်ဖက်မှာရှိတယ်ဆိုဘယ်ကိုတောက်လျောက် RecursiveCallပြီး ရှာမှာဖြစ်ပါတယ်
+         return Comparator(arr,left,mid-1,target) 
+     }
+
+     return Comparator(arr,mid+1,right,target)// ဒီနေရာကတော့ ကိုဘ်ရှာချင်တဲ့ Target ညာဖက်မှာရှိတယ်ဆိုဘယ်ကိုတောက်လျောက်RecursiveCallပြီး ရှာမှာဖြစ်ပါတယ်
+    
+     
+ }
+
+    
+ if(nums.length === 0){
+     return 0
+ }else{
+
+     return Comparator(nums,0,nums.length-1,target)
+ }   
+    
+};
