c370bad711d8cd34e567a2293913bca9eeff42b7
win Htaik Aung <winhtaikaung28@hotmail.com>
2024-02-12 12:19:29 +0000

    problem 3 solved

---
+++ lc/python/3_longest-substring-without-repeating-characters.py
@@ -0,0 +1,35 @@
+# Given the problem,
+# https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+# Generate pytest test cases in pytest.mark.parametrize way.
+
+import pytest
+
+def length_of_longest_substring(s):
+    # Solution function goes here
+    kv = set()
+    charlist= [x for x in s]
+    l , res = 0,0
+    for k,v in enumerate(charlist):
+        while v in kv:
+            kv.remove(charlist[l])
+            l+=1
+        
+        kv.add(v)   # add item to kv
+        res = max(res,k-l+1)
+    return res
+
+@pytest.mark.parametrize("s, expected", [
+    ("abcabcbb", 3),
+    ("bbbbb", 1),
+    ("pwwkew", 3),
+    ("", 0),
+    (" ", 1),
+    ("abcdefg", 7),
+    ("aab", 2),
+    ("dvdf", 3),
+    ("tmmzuxt", 5),
+    ("aabaab!bb", 3)
+])
+def test_length_of_longest_substring(s, expected):
+    assert length_of_longest_substring(s) == expected
